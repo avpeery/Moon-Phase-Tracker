4d72ab9232066aae1b7e55f2803796450c484d33
Anna Peery <avpeery@gmail.com>
2019-11-05 23:56:52 +0000

    Added classes User, Moon Phase, and Alert to model.py

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 """Models and database functions for Moon Phase Tracker project"""
 from flask_sqlalchemy import SQLAlchemy  
+from datetime import datetime
 
 #initialize SQL Alchemy object
 db = SQLAlhemy()
@@ -8,7 +9,8 @@ db = SQLAlhemy()
 
 class User(db.Model):
     """User of Moon Phase Tracker Web App"""
-    __tablename__ = 'users'
+
+    __tablename__ = "users"
 
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     fname = db.Column(db.String(64), nullable = True)
@@ -22,4 +24,60 @@ class User(db.Model):
     last_quarter = db.Column(db.Boolean(False))
     full_moon = db.Column(db.Boolean(False))
 
-class 
+    def __repr__(self):
+
+        return f"<User user_id={self.user_id} email={self.email}>"
+
+
+class Moon_Phase(db.Model):
+    """Moon Phase of Moon Phase Tracker Web App"""
+
+    __tablename__ = "moon_phases"
+
+    moon_phase_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String(64))
+    start_date = db.Column(db.DateTime)
+    end_date = db.Column(db.DateTime)
+
+    def __repr__(self):
+
+        return f"<Moon_Phase moon_phase_id={self.moon_phase_id} name={self.name}>"
+
+
+class Alert(db.Model):
+
+    __tablename__ = "alerts"
+
+    alert_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    users_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    moon_phase_id = db.Column(db.Integer, db.ForeignKey('moon_phases.moon_phase_id'))
+    user = db.relationship("User", backref=db.backref("alerts"))
+    movie = db.relationship("Moon_Phase", backref=db.backref("alerts"))
+
+    def __repr__(self):
+
+        return f"<Alert alert_id={self.alert_id} user_id={self.user_id} moon_phase_id={self.moon_phase_id}>"
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///moon_phases'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_ECHO'] = True
+
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # to work with interactively
+
+    from server import app
+    connect_to_db(app)
+    print("Connected to DB.")
+
+
+
+
+
+
