51d45fc6d6ea1eb9397e3927c0cd9359436fe40b
Anna Peery <avpeery@gmail.com>
2019-11-19 20:12:41 +0000

    Added event click to calendar events

--- static/calendar.js
+++ static/calendar.js
@@ -5,8 +5,12 @@ document.addEventListener('DOMContentLoaded', function() {
     var calendar = new FullCalendar.Calendar(calendarEl, {
         plugins: [ 'dayGrid' ],
         events: res,
-});
+        eventClick: function(events) {
+            alert('Event: ' + events.event.title);
+            alert('Coordinates: ' + events.jsEvent.pageX + ',' + events.jsEvent.pageY);
+            alert('View: ' + events.view.type)
+        }
+    });
     calendar.render();
 });
-
 });
