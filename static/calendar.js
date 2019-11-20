0ae43841fd119562355a3e2fc480ad17e11e7778
Anna Peery <avpeery@gmail.com>
2019-11-20 01:37:13 +0000

    Hid start times on calendar events

--- static/calendar.js
+++ static/calendar.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
         var calendar = new FullCalendar.Calendar(calendarEl, {
             plugins: [ 'dayGrid' ],
             events: res,
+            displayEventTime: false,
             eventRender: function(info) {
               var tooltip = new Tooltip(info.el, {
                 title: info.event.title,
