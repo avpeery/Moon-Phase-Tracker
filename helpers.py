0ff623d5c273f01a7fa7cea736308d46e53e9325
Anna Peery <avpeery@gmail.com>
2019-12-06 07:27:28 +0000

    Fixed unexpected indent in helpers.py

--- helpers.py
+++ helpers.py
@@ -5,23 +5,23 @@ from flask import Flask, request
 def form_get_request(*argv):
   """Receives multiple items from post request, processes, and returns list"""
 
-    form_get_request_list = []
+  form_get_request_list = []
 
-    for arg in argv:
-        form_get_request_list.append(request.form.get(arg))
+  for arg in argv:
+    form_get_request_list.append(request.form.get(arg))
 
-    return form_get_request_list
+  return form_get_request_list
 
 
 def form_get_list(*argv):
   """Receives list items (ex: checkboxes) from post request, processes, and returns nested list"""
 
-    form_get_nested_list = []
+  form_get_nested_list = []
 
-    for arg in argv:
-        form_get_nested_list.append(request.form.getlist(arg))
+  for arg in argv:
+    form_get_nested_list.append(request.form.getlist(arg))
 
-    return form_get_nested_list
+  return form_get_nested_list
 
 
 def credentials_to_dict(credentials):
